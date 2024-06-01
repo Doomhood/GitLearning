06434f5388e91e39d97197fc17065fef61ee0024
Yang Jiale <2871217422@qq.com>
2024-06-01 19:52:36 +0800

    add string tips

--- file01.py
+++ file01.py
@@ -3,8 +3,11 @@ import file03 as f3
 import file02 as f2
 
 
+
 if __name__ == '__main__':
-    print(f3.add(1, 2))
-    print("Hello World!!")
     print("This is the main program")
-    print(f2.add(10, 20))
+    print(f"The result of file03.add(1, 2) is:{f3.add(1, 2)}")
+    print(f"The result of file02.add(10, 20) is:{f2.add(10, 20)}")
+    print("This is the end of the program")
+
+
